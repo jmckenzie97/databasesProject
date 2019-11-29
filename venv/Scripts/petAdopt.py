3c341a8c84d03459085b587dc26235488460fab3
jmckenzie97 <49374308+jmckenzie97@users.noreply.github.com>
2019-11-28 23:16:10 -0500

    added login support

--- venv/Scripts/petAdopt.py
+++ venv/Scripts/petAdopt.py
@@ -1,22 +1,21 @@
 import pyrebase
-from flask import Flask, render_template, redirect, url_for, request
-from flask import json
-from string import Template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-# # ---- Pyrebase config ---- #
-# config = {
-#     "apiKey": "AIzaSyDLDdQ_qcgm6R4CU2iPesBDt_etoJEnY-k",
-#     "authDomain": "hippodrome-e434f.firebaseapp.com",
-#     "databaseURL": "https://hippodrome-e434f.firebaseio.com",
-#     "projectId": "hippodrome-e434f",
-#     "storageBucket": "",
-#     "messagingSenderId": "295932205335",
-#     "appId": "1:295932205335:web:99122bf40d9b27c2"
-# }
-# firebase = pyrebase.initialize_app(config)
-# auth = firebase.auth()
+app.secret_key = 'your secret key'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'petFinder'
+
+# Intialize MySQL
+mysql = MySQL(app)
 
 # ---- route function ---- #
 ''' Decorator which tells the 
@@ -26,15 +25,35 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 # HTTP methods GET and POST as argument in route decorator
 @app.route("/", methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form['username']
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
         password = request.form['password']
-        try:
-            # auth.sign_in_with_email_and_password(email, password)
-            return redirect('/home')
-        except:
-            return 'Login Failed.'
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return redirect(url_for('home'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
     return render_template('login.html')
+
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
 # ---- run method ---- #
 if __name__ == "__main__":
